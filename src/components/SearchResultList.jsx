9ee168f8d7d681a82ae6c12274ce7a98887ad08d
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-05-31 02:06:09 +0200

    add external API  Search functions

--- src/components/SearchResultList.jsx
+++ src/components/SearchResultList.jsx
@@ -1,30 +1,20 @@
-const SearchResultList = ({ searchResult }) => {
-    console.log('Movie', searchResult)
+const SearchResultList = ({ movieList }) => {
     
     return (
-        
-            searchResult.Response === 'True'
-            ?
-                <>
-                    <p>
-                        <span>Total results: {searchResult.totalResults}</span>
-                        {searchResult.totalResults > 10 && <span> Page 1 of {Math.ceil(searchResult.totalResults / 10)}</span>}
-                    </p>
-                    <ul>
-                        {searchResult.Search.map(movie => {                           
-                            
-                            return (
-                                <li key={movie.imdbID}>
-                                    <span className="text-gray-500">{movie.Year}</span>
-                                    <span className="font-semibold"> {movie.Title}</span>
-                                    <span className="text-sm"> {movie.Type}</span>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </>
-            :
-                <p>Error: {searchResult.Error}</p>        
+        <ul className="grid gap-1">
+            {movieList.map(movie => {                           
+                
+                return (
+                    <li key={movie.imdbID} className="grid gap-4 grid-cols-[9ch_1fr] items-center bg-white px-2 py-1 rounded-md shadow-sm">
+                        <div className="grid justify-items-center">
+                            <div className="text-gray-500">{movie.Year}</div>
+                            <div className="text-xs"> {movie.Type}</div>
+                        </div>
+                        <div className="font-semibold text-balance"> {movie.Title}</div>
+                    </li>
+                )
+            })}
+        </ul>  
     )
 }
 
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -3,18 +3,27 @@ import { useState } from 'react'
 import SearchResultList from '../components/SearchResultList';
 
 const SearchPage = () => {
-    const [searchResult, setSearchResult] = useState(null)
-    const [inputString, setInputString] = useState('')
+    const [searchResult, setSearchResult] = useState(null);
+    const [inputString, setInputString] = useState('');
+    const [curPage, setCurPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+
     // const apiKey = import.meta.env.VITE_OMDB_API_KEY
     const apiKey = import.meta.env.VITE_OMDB_API_KEY_II
-    const apiURL = `https://www.omdbapi.com/?apikey=${apiKey}`
+    const apiURL = `https://www.omdbapi.com/?apikey=${apiKey}`;
 
-    const fetchSearchResults = async (searchString) => {
+    const borderStyle = "border border-gray-300 bg-sky-800 text-white px-4 py-2 rounded-md";
+
+    const fetchSearchResults = async (searchString, page = 1) => {
         const urlString = encodeURIComponent(searchString.trim().toLowerCase())
+        const urlPage = page > 1 ? `&page=${page}` : ''
         try {
-            const response = await fetch(`${apiURL}&s=${urlString}`)
+            const response = await fetch(`${apiURL}&s=${urlString}${urlPage}`)
             const data = await response.json()
             setSearchResult(data)
+            if  (data.Response === 'True' && data.totalResults > 10) {
+                setTotalPages(Math.ceil(data.totalResults / 10))
+            }
         } catch (error) {
             console.error(error)
         }
@@ -22,29 +31,21 @@ const SearchPage = () => {
 
     const searchFormHandler = (e) => {
         e.preventDefault()
-        const value = e.target.title.value
-        fetchSearchResults(value)
+        // const value = e.target.title.value
+        fetchSearchResults(inputString)
     }
-    
-    // useEffect(() => {
-    //     const timer = setTimeout(() => {
-    //         setSearchResult([
-    //             { title: 'The Shawshank Redemption', year: 1994 },
-    //             { title: 'The Godfather', year: 1972 },
-    //             { title: 'The Godfather: Part II', year: 1974 },
-    //             { title: 'The Dark Knight', year: 2008 },
-    //             { title: '12 Angry Men', year: 1957 },
-    //         ])
-    //     }, 3000)
-    //     return () => {
-    //         clearTimeout(timer)
-    //     }    
-    // }, [])
+
+    const changePageHandler = (page) => {
+        if (page > 0 && page <= totalPages) {
+            setCurPage(page)
+            fetchSearchResults(inputString, page)
+
+        }
+    }
 
     return (
         <section className="container mx-auto p-4">
             <h2 className="text-3xl font-bold my-4 text-center">Search movie in OMDB</h2>
-            <p className="my-2 text-end">It&apos;s not ready keep in touch</p>
             <form onSubmit={searchFormHandler}  className="flex justify-start items-end gap-2 my-4">
                 <label className="block">Search movie
                     <input
@@ -55,12 +56,30 @@ const SearchPage = () => {
                         className="w-80 block border border-gray-300 rounded-md px-4 py-2"
                     />
                 </label>
-                <button type="submit" className="border border-gray-300 bg-blue-500 text-white px-4 py-2 rounded-md">Search</button>
+                <button type="submit" className={borderStyle}>Search</button>
             </form>
 
-            { searchResult
-                ? <SearchResultList searchResult={searchResult} />
-                : <p className="text-center">No results found</p>
+            {
+                searchResult
+                && (
+                    searchResult.Response === 'True'
+                    ?   <div className="grid grid-cols-1 md:grid-cols-[3fr_2fr] gap-10">
+                            <div>
+                                <p className='my-4'>Total results: {searchResult.totalResults}</p>
+                                <SearchResultList movieList={searchResult.Search} />
+                                {totalPages > 1
+                                    && <>
+                                            <div className='my-4 flex gap-12 justify-between items-center'>
+                                                <button inert={curPage <= 1 ? '' : null} onClick={() => changePageHandler(curPage - 1)} className={borderStyle}>Prev</button>
+                                            <p> Page {curPage} of {totalPages}</p>
+                                                <button inert={curPage >= totalPages ? '' : null} onClick={() => changePageHandler(curPage + 1)} className={borderStyle}>Next</button>
+                                            </div>
+                                        </>
+                                }
+                            </div>
+                        </div>
+                    :   <p>Error: {searchResult?.Error}</p>
+                )
             }
 
         </section>
